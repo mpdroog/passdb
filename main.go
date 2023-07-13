82a489129bb106ad147151069ab8155ab892431c
mark <mpdroog@rootdev.nl>
2023-07-13 16:05:05 +0200

    Feature. Better CLI-parsing, add import and find entries

--- main.go
+++ main.go
@@ -3,13 +3,15 @@ package main
 import (
 	"bufio"
 	"crypto/sha256"
+	"encoding/csv"
 	"encoding/json"
 	"errors"
-	"flag"
 	"fmt"
+	"github.com/docopt/docopt-go"
 	"github.com/mpdroog/passdb/stream"
 	"golang.org/x/crypto/scrypt"
 	"golang.org/x/term"
+	"io"
 	"math/rand"
 	"os"
 	"strings"
@@ -25,9 +27,12 @@ type Cred struct {
 	User string
 	Pass string
 	Meta string
+	Type string
+	URL  string
 }
 
 var (
+	DBPath  string
 	Verbose bool
 	letters = []byte("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()")
 
@@ -59,9 +64,11 @@ func parseFile(bytePassword []byte, fname string, out interface{}) error {
 		}
 	}()
 
+	r := bufio.NewReader(fd)
+
 	// Read nonce from first 8 bytes
 	nonce := make([]byte, 8)
-	n, e := fd.Read(nonce)
+	n, e := r.Read(nonce)
 	if e != nil {
 		return e
 	}
@@ -70,12 +77,12 @@ func parseFile(bytePassword []byte, fname string, out interface{}) error {
 	}
 
 	privKey, e := scryptKey(bytePassword, ([8]byte)(nonce))
-	r, e := stream.NewReader(privKey, fd)
+	rs, e := stream.NewReader(privKey, r)
 	if e != nil {
 		return e
 	}
 
-	if e := json.NewDecoder(r).Decode(out); e != nil {
+	if e := json.NewDecoder(rs).Decode(out); e != nil {
 		return e
 	}
 
@@ -93,7 +100,9 @@ func writeFile(nonce []byte, privKey []byte, path string, f interface{}) error {
 		}
 	}()
 
-	n, e := fd.Write(nonce)
+	w := bufio.NewWriter(fd)
+
+	n, e := w.Write(nonce)
 	if e != nil {
 		return e
 	}
@@ -101,17 +110,17 @@ func writeFile(nonce []byte, privKey []byte, path string, f interface{}) error {
 		return fmt.Errorf("Failed writing nonce")
 	}
 
-	w, e := stream.NewWriter(privKey, fd)
+	sw, e := stream.NewWriter(privKey, w)
 	if e != nil {
 		return e
 	}
 	defer func() {
-		if e := w.Close(); e != nil {
+		if e := sw.Close(); e != nil {
 			fmt.Printf("writeFile.Close2 e=%s\n", e.Error())
 		}
 	}()
 
-	if e := json.NewEncoder(w).Encode(f); e != nil {
+	if e := json.NewEncoder(sw).Encode(f); e != nil {
 		return e
 	}
 	return nil
@@ -125,27 +134,102 @@ func getStdin(question string) (string, error) {
 	return s, e
 }
 
-func main() {
-	rand.Seed(time.Now().UnixNano())
-	dbPath := ""
-	flag.BoolVar(&Verbose, "v", false, "Show all that happens")
-	flag.StringVar(&dbPath, "d", "./creds.d", "Path to credentials-dir")
-	flag.Parse()
-
-	if len(os.Args) < 2 {
-		fmt.Printf("Example usage:\n")
-		fmt.Printf("\t%s get github\n", os.Args[0])
-		fmt.Printf("\t%s add gitlab\n", os.Args[0])
-		fmt.Printf("\t%s export gitlab\n", os.Args[0])
-		os.Exit(1)
-		return
+func add(name string, bytePassword []byte, cred Cred) {
+	var hname string
+	{
+		h := sha256.New()
+		h.Write([]byte(name))
+		hname = fmt.Sprintf("%x", h.Sum(nil))
 	}
 
-	file, e := os.Open(dbPath)
+	fname := fmt.Sprintf("%s/%s.json.enc", DBPath, hname)
+	c := File{}
+
+	if _, e := os.Stat(fname); e == nil {
+		if e := parseFile(bytePassword, fname, &c); e != nil {
+			panic(e)
+		}
+	} else if !errors.Is(e, os.ErrNotExist) {
+		// Only panic when error something else than nonexists
+		panic(e)
+	}
+
+	c.Creds = append(c.Creds, cred)
+	if Verbose {
+		fmt.Printf("Write=%+v\n", c)
+	}
+
+	nonce := randSeq(8)
+	privKey, e := scryptKey(bytePassword, ([8]byte)(nonce))
 	if e != nil {
 		panic(e)
 	}
-	defer file.Close()
+	if e := writeFile(nonce, privKey, fname, &c); e != nil {
+		panic(e)
+	}
+
+	// Now also update Lookup
+	{
+		nonce := randSeq(8)
+		privKey, e := scryptKey(bytePassword, ([8]byte)(nonce))
+		if e != nil {
+			panic(e)
+		}
+		Lookup[name] = hname
+		if e := writeFile(nonce, privKey, DBPath+"/lookup.json.enc", Lookup); e != nil {
+			panic(e)
+		}
+	}
+}
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+	usage := `Passdb.
+  Password manager that optimises for easily distributing your passwords.
+
+Usage:
+  passdb find <name> [--verbose] [--dir=<dir>]
+  passdb get <name> [--verbose] [--dir=<dir>]
+  passdb add <name> [--verbose] [--dir=<dir>]
+  passdb import <file> [--verbose] [--dir=<dir>]
+  passdb export [--verbose] [--dir=<dir>]
+  passdb -h | --help
+
+Options:
+  -h --help           Show this screen.
+  -v --verbose        Verbose mode.
+  -d --dir=<dir>      Credentials-dir [default: ./creds.d].`
+
+	args, e := docopt.ParseDoc(usage)
+	if e != nil {
+		panic(e)
+	}
+	Verbose, e = args.Bool("--verbose")
+	if e != nil {
+		panic(e)
+	}
+	if Verbose {
+		fmt.Println(args)
+	}
+	DBPath, e = args.String("--dir")
+	if e != nil {
+		panic(e)
+	}
+
+	cmd := ""
+	// TODO: Kind of duplicate
+	for _, k := range []string{"find", "get", "add", "import", "export"} {
+		if ok, _ := args.Bool(k); ok {
+			cmd = k
+			break
+		}
+	}
+	fname, _ := args.String("<name>")
+	if len(fname) == 0 {
+		// Kind of lazy
+		fname, _ = args.String("<file>")
+	}
+	fname = strings.ToLower(fname)
 
 	// TODO: protect privKey in memory?
 	var bytePassword []byte
@@ -159,13 +243,13 @@ func main() {
 
 	// Lookup-tbl
 	{
-		fname := fmt.Sprintf("%s/lookup.json.enc", dbPath)
+		fname := fmt.Sprintf("%s/lookup.json.enc", DBPath)
 		haveFile := true
-		if _, e := os.Stat(fname); errors.Is(e, os.ErrNotExist) {
+		_, e := os.Stat(fname)
+		if errors.Is(e, os.ErrNotExist) {
 			Lookup = make(map[string]string)
 			haveFile = false
-		}
-		if e != nil {
+		} else if e != nil {
 			panic(e)
 		}
 
@@ -176,18 +260,7 @@ func main() {
 		}
 	}
 
-	var fname string
-	var hash string
-	{
-		h := sha256.New()
-		h.Write([]byte(os.Args[2]))
-		hash = fmt.Sprintf("%x", h.Sum(nil))
-		fname = fmt.Sprintf("%s/%s.json.enc", dbPath, hash)
-	}
-
-	if os.Args[1] == "add" {
-		c := File{}
-
+	if cmd == "add" {
 		user, e := getStdin("user")
 		if e != nil {
 			panic(e)
@@ -201,49 +274,58 @@ func main() {
 			panic(e)
 		}
 
-		if _, err := os.Stat(fname); err == nil {
-			if e := parseFile(bytePassword, fname, &c); e != nil {
-				panic(e)
-			}
-		} else if !errors.Is(err, os.ErrNotExist) {
-			// Only panic when error something else than nonexists
-			panic(e)
-		}
+		add(fname, bytePassword, Cred{User: user, Pass: pass, Meta: meta})
 
-		c.Creds = append(c.Creds, Cred{User: user, Pass: pass, Meta: meta})
+	} else if cmd == "import" {
+		// Login = "xyz","cointracker.io","Login","cointracker.io","mail@domain.com",
+		// Wireless Router = "passss","Networkname","Wireless Router",,,
+		// Bank Account = "1234","Bank Business","Bank Account",,,
+		// Password = "xxx","eBay","Password","ebay.com",,
 		if Verbose {
-			fmt.Printf("Write=%+v\n", c)
+			fmt.Printf("import=%s\n", fname)
 		}
-
-		nonce := randSeq(8)
-		privKey, e := scryptKey(bytePassword, ([8]byte)(nonce))
+		fd, e := os.Open(fname)
 		if e != nil {
 			panic(e)
 		}
-		if e := writeFile(nonce, privKey, fname, &c); e != nil {
-			panic(e)
-		}
+		defer fd.Close()
 
-		// Now also update Lookup
-		{
-			nonce := randSeq(8)
-			privKey, e := scryptKey(bytePassword, ([8]byte)(nonce))
+		// TODO: Cache file through bufferreader?
+		r := csv.NewReader(fd)
+
+		scanner := bufio.NewScanner(fd)
+		// optionally, resize scanner's capacity for lines over 64K, see next example
+		for {
+			toks, e := r.Read()
+			if e == io.EOF {
+				break
+			}
 			if e != nil {
 				panic(e)
 			}
-			Lookup[os.Args[2]] = hash
-			if e := writeFile(nonce, privKey, dbPath+"/lookup.json.enc", Lookup); e != nil {
-				panic(e)
+			if Verbose {
+				fmt.Printf("%+v\n", toks)
 			}
+
+			key := strings.ReplaceAll(strings.ToLower(toks[1]), " ", "_")
+			c := Cred{User: toks[4], Pass: toks[0], Meta: toks[1], URL: toks[3], Type: toks[2]}
+			if Verbose {
+				fmt.Printf("C(key=%s)=%+v\n", key, c)
+			}
+			add(key, bytePassword, c)
 		}
 
-	} else if os.Args[1] == "export" {
+		if e := scanner.Err(); e != nil {
+			panic(e)
+		}
+
+	} else if cmd == "export" {
 		if Verbose {
 			fmt.Printf("lookup=%+v\n", Lookup)
 		}
 		for name, fname := range Lookup {
-			fullFname := fmt.Sprintf("%s/%s.json.enc", dbPath, fname)
-			fmt.Printf("%s\n=======================\n", name)
+			fullFname := fmt.Sprintf("%s/%s.json.enc", DBPath, fname)
+			fmt.Printf("\n%s\n=======================\n", name)
 			var creds = File{}
 			if e := parseFile(bytePassword, fullFname, &creds); e != nil {
 				panic(e)
@@ -252,16 +334,51 @@ func main() {
 				fmt.Printf("user=%s\n", cred.User)
 				fmt.Printf("pass=%s\n", cred.Pass)
 				fmt.Printf("meta=%s\n", cred.Meta)
+				fmt.Printf("url=%s\n", cred.URL)
 				if id+1 != len(creds.Creds) {
 					fmt.Printf("\n")
 				}
 			}
 		}
 
-	} else if os.Args[1] == "get" {
+	} else if cmd == "find" {
+		for name, filename := range Lookup {
+			if !strings.Contains(name, fname) {
+				// Keyname does not match
+				continue
+			}
+			var creds = File{}
+			fullFname := fmt.Sprintf("%s/%s.json.enc", DBPath, filename)
+			if e := parseFile(bytePassword, fullFname, &creds); e != nil {
+				panic(e)
+			}
+			fmt.Printf("\n%s\n=======================\n", name)
+			for id, cred := range creds.Creds {
+				fmt.Printf("user=%s\n", cred.User)
+				fmt.Printf("pass=%s\n", cred.Pass)
+				fmt.Printf("meta=%s\n", cred.Meta)
+				fmt.Printf("url=%s\n", cred.URL)
+				if id+1 != len(creds.Creds) {
+					fmt.Printf("\n")
+				}
+			}
+		}
+
+	} else if cmd == "get" {
+		var fname string
+		var hash string
+		{
+			h := sha256.New()
+			h.Write([]byte(fname))
+			hash = fmt.Sprintf("%x", h.Sum(nil))
+			fname = fmt.Sprintf("%s/%s.json.enc", DBPath, hash)
+		}
+
 		if Verbose {
 			fmt.Printf("Read=%s\n", fname)
 		}
+		// TODO: Maybe suggest if file not exists?
+
 		var creds = File{}
 		if e := parseFile(bytePassword, fname, &creds); e != nil {
 			panic(e)
@@ -270,13 +387,14 @@ func main() {
 			fmt.Printf("user=%s\n", cred.User)
 			fmt.Printf("pass=%s\n", cred.Pass)
 			fmt.Printf("meta=%s\n", cred.Meta)
+			fmt.Printf("url=%s\n", cred.URL)
 			if id+1 != len(creds.Creds) {
 				fmt.Printf("\n")
 			}
 		}
 
 	} else {
-		fmt.Printf("Invalid args\n")
+		fmt.Printf("No such cmd=%s\n", cmd)
 		os.Exit(1)
 		return
 	}
